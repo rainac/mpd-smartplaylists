21839acacbfa746ff9164cc82eff45d5c77c3d2a
Johannes Willkomm <johannes@johannes-willkomm.de>
2015-08-19 22:46:08 +0200

    Add more tests for nested parentheses and/or expressions

--- tests/test_parser.sh
+++ tests/test_parser.sh
@@ -48,6 +48,14 @@ test_or3() {
     assertEquals "0" "$?"
 }
 
+test_and_or1() {
+    cmd=$($SMPL_HOME/smartplaylist.sh -m spsh artist=bad or '(' album=god or title=child ')' and title=t | tee tmp.sh)
+    echo "mpc search  artist bad
+    mpc search  album god title t
+    mpc search  title child title t" | diff -B -w - tmp.sh
+    assertEquals "0" "$?"
+}
+
 test_and_nested_or_query() {
     cmd=$($SMPL_HOME/smartplaylist.sh -m spsh artist=bad and '(' album=god or album=rise ')' | tee tmp.sh)
     echo "mpc search  artist bad album god
@@ -68,16 +76,18 @@ test_and_nested_or_query() {
 test_and_nested_or_query2() {
     cmd=$($SMPL_HOME/smartplaylist.sh -m spsh artist=bad and '(' album=god or album=rise ')' and '(' title=miss or title=child ')' | tee tmp.sh)
     echo "mpc search  artist bad album god title miss
-    mpc search  artist bad album rise title miss" | diff -B -w - tmp.sh
+    mpc search  artist bad album god title child
+    mpc search  artist bad album rise title miss
+    mpc search  artist bad album rise title child" | diff -B -w - tmp.sh
     assertEquals "0" "$?"
 }
 
 test_and_nested_or_query3() {
     cmd=$($SMPL_HOME/smartplaylist.sh -m spsh artist=bad and '(' album=god or artist=good and '(' album=devil or album=rise ')' ')' | tee tmp.sh)
     echo "mpc search  artist bad album god
-    mpc search  artist bad album rise" | diff -B -w - tmp.sh
+    mpc search  artist bad artist good album devil
+    mpc search  artist bad artist good album rise" | diff -B -w - tmp.sh
     assertEquals "0" "$?"
-
 }
 
 . shunit2

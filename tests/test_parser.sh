7c9314fc808244c3e8a8ab7eecc8fcfbb2c5aedb
Johannes Willkomm <johannes@johannes-willkomm.de>
2015-08-26 21:49:42 +0200

    Adapt tests to the new output (parentheses of subshell in or)

--- tests/test_parser.sh
+++ tests/test_parser.sh
@@ -20,7 +20,7 @@ test_and_query() {
 }
 test_or_query() {
     cmd=$($SMPL_HOME/smartplaylist.sh -m spsh album=a or album=b | tee tmp.sh)
-    echo -e "mpc search album a\nmpc search album b" | diff -B -w - tmp.sh
+    echo -e "(\nmpc search album a\nmpc search album b)" | diff -B -w - tmp.sh
     assertEquals "0" "$?"
 }
 
@@ -42,59 +42,68 @@ test_and3() {
 
 test_or3() {
     cmd=$($SMPL_HOME/smartplaylist.sh -m spsh artist=bad or album=god or title=child | tee tmp.sh)
-    echo "mpc search  artist bad
+    echo "(
+    mpc search  artist bad
     mpc search  album god
-    mpc search  title child" | diff -B -w - tmp.sh
+    mpc search  title child)" | diff -B -w - tmp.sh
     assertEquals "0" "$?"
 }
 
 test_and_or1() {
-    cmd=$($SMPL_HOME/smartplaylist.sh -m spsh artist=bad or '(' album=god or title=child ')' and title=t | tee tmp.sh)
-    echo "mpc search  artist bad
+    cmd=$($SMPL_HOME/smartplaylist.sh -m spsh artist = bad or '(' album=god or title=child ')' and title=t | tee tmp.sh)
+    echo "(
+    mpc search  artist bad
     mpc search  album god title t
-    mpc search  title child title t" | diff -B -w - tmp.sh
+    mpc search  title child title t)" | tee tmp2.sh | diff -B -w - tmp.sh
     assertEquals "0" "$?"
+    rm tmp.sh tmp2.sh
 }
 
 test_and_nested_or_query() {
     cmd=$($SMPL_HOME/smartplaylist.sh -m spsh artist=bad and '(' album=god or album=rise ')' | tee tmp.sh)
-    echo "mpc search  artist bad album god
-    mpc search  artist bad album rise" | diff -B -w - tmp.sh
+    echo "(
+    mpc search  artist bad album god
+    mpc search  artist bad album rise)" | diff -B -w - tmp.sh
     assertEquals "0" "$?"
 
     cmd=$($SMPL_HOME/smartplaylist.sh -m spsh artist=bad and '(' album=god or album=rise ')' and title=miss| tee tmp.sh)
-    echo "mpc search  artist bad album god title miss
-    mpc search  artist bad album rise title miss" | diff -B -w - tmp.sh
+    echo "(
+    mpc search  artist bad album god title miss
+    mpc search  artist bad album rise title miss)" | diff -B -w - tmp.sh
     assertEquals "0" "$?"
 
     cmd=$($SMPL_HOME/smartplaylist.sh -m spsh '(' album=god or album=rise ')' and title=miss| tee tmp.sh)
-    echo "mpc search  album god title miss
-    mpc search  album rise title miss" | diff -B -w - tmp.sh
+    echo "(
+    mpc search  album god title miss
+    mpc search  album rise title miss)" | diff -B -w - tmp.sh
     assertEquals "0" "$?"
 }
 
 test_and_nested_or_query2() {
     cmd=$($SMPL_HOME/smartplaylist.sh -m spsh artist=bad and '(' album=god or album=rise ')' and '(' title=miss or title=child ')' | tee tmp.sh)
-    echo "mpc search  artist bad album god title miss
+    echo "(
+    mpc search  artist bad album god title miss
     mpc search  artist bad album god title child
     mpc search  artist bad album rise title miss
-    mpc search  artist bad album rise title child" | diff -B -w - tmp.sh
+    mpc search  artist bad album rise title child)" | diff -B -w - tmp.sh
     assertEquals "0" "$?"
 }
 
 test_and_nested_or_query3() {
     cmd=$($SMPL_HOME/smartplaylist.sh -m spsh artist=bad and '(' album=god or artist=good and '(' album=devil or album=rise ')' ')' | tee tmp.sh)
-    echo "mpc search  artist bad album god
+    echo "(
+    mpc search  artist bad album god
     mpc search  artist bad artist good album devil
-    mpc search  artist bad artist good album rise" | diff -B -w - tmp.sh
+    mpc search  artist bad artist good album rise)" | diff -B -w - tmp.sh
     assertEquals "0" "$?"
 }
 
 test_and_nested_or_query3a() {
     cmd=$($SMPL_HOME/smartplaylist.sh -m spsh artist=bad and '((((' album=god or artist=good and '((((' album=devil or album=rise '))))' '))))' | tee tmp.sh)
-    echo "mpc search  artist bad album god
+    echo "(
+    mpc search  artist bad album god
     mpc search  artist bad artist good album devil
-    mpc search  artist bad artist good album rise" | diff -B -w - tmp.sh
+    mpc search  artist bad artist good album rise)" | diff -B -w - tmp.sh
     assertEquals "0" "$?"
 }
 

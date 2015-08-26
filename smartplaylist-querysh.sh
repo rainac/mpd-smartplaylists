2e58cf946ca033e44c5bd44b29ac7e626c9da0c4
Johannes Willkomm <johannes@johannes-willkomm.de>
2015-08-26 21:49:16 +0200

    Use the new smartplaylist-distrib-or-over-and.sh script as a building stone

--- smartplaylist-querysh.sh
+++ smartplaylist-querysh.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
 
 res=0
-cat - > in.xml;
-while ! xsltproc $SMPL_HOME/smartplaylist-has-paren.xsl in.xml >& /dev/null; do
-    xsltproc -o in-tmp.xml $SMPL_HOME/smartplaylist-distrib-or.xsl in.xml
-    cp in-tmp.xml in.xml
-done
 
-xsltproc $SMPL_HOME/genupdate-sh.xsl in.xml
+cat - | smartplaylist-distrib-or-over-and.sh | xsltproc $SMPL_HOME/genupdate-sh.xsl -
+
 exit $res

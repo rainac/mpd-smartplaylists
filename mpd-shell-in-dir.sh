a7e136107639e685f6a17d31db09f1208c567498
rainac <johannes@johannes-willkomm.de>
2016-08-12 16:11:11 +0200

    New utility script

---
+++ mpd-shell-in-dir.sh
@@ -0,0 +1,21 @@
+#! /bin/zsh
+
+res=0
+nl=${1:-1} # number in playlist
+
+
+dir=$(dirname "$(mpc -f '%file%' playlist | head -n "$nl" | tail -n 1)")
+
+cat > rcfile <<EOF
+source ~/.local.env
+cd "\$MPD_ROOT/$dir"
+$SHELL -i
+EOF
+
+scp -q rcfile $MPD_HOST:
+ssh -t $MPD_HOST source rcfile
+
+ssh $MPD_HOST rm rcfile
+rm rcfile
+
+exit res

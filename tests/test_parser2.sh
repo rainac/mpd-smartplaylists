6fd8aa12e84215c5b5c237800c98ec4537e38912
rainac <johannes@johannes-willkomm.de>
2016-08-21 21:11:21 +0200

    Some new tests

--- tests/test_parser2.sh
+++ tests/test_parser2.sh
@@ -44,6 +44,62 @@ test_simple_filter_query6() {
     assertEquals "0" "$?"
 }
 
+test_filter_prec_and() {
+    cmd=$($SMPL_HOME/smartplaylist.sh -m spsh "abc and def<ttt" | tee tmp.sh)
+    echo "mpc search artist abc artist def|grep -i \"ttt\"" | diff -B -w - tmp.sh
+    assertEquals "0" "$?"
+}
+
+test_filter_prec_or() {
+    cmd=$($SMPL_HOME/smartplaylist.sh -m spsh "abc or def<ttt" | tee tmp.sh)
+    echo -e "(\n\
+           mpc search artist abc \n\
+           mpc search artist def)|grep -i \"ttt\"" | diff -B -w - tmp.sh
+    assertEquals "0" "$?"
+}
+
+test_filter_prec_paren() {
+    cmd=$($SMPL_HOME/smartplaylist.sh -m spsh "(abc>t11) or (def<t22)" | tee tmp.sh)
+    echo -e "(\n\
+           mpc search artist abc|grep -i -v \"t11\" \n\
+           mpc search artist def|grep -i \"t22\")" | diff -B -w - tmp.sh
+    assertEquals "0" "$?"
+}
+
+test_filter_prec_and_or() {
+    cmd=$($SMPL_HOME/smartplaylist.sh -m spsh "(abc and def>t11) or (ghi and jkl<t22)" | tee tmp.sh)
+    echo -e "(\n\
+           mpc search artist abc artist def|grep -i -v \"t11\" \n\
+           mpc search artist ghi artist jkl|grep -i \"t22\")" | diff -B -w - tmp.sh
+    assertEquals "0" "$?"
+}
+
+_test_filter_prec_and_or_error1() {
+    cmd=$($SMPL_HOME/smartplaylist.sh -m spsh "(abc and def>t11) and (ghi and jkl<t22)" | tee tmp.sh)
+    echo -e "(\n\
+           mpc search artist abc|grep -i -v \"t11\" \n\
+           mpc search artist def|grep -i \"t22\")" | diff -B -w - tmp.sh
+    assertEquals "0" "$?"
+}
+
+_test_filter_prec_and_or_error2() {
+    cmd=$($SMPL_HOME/smartplaylist.sh -m spsh "(abc or def>t11) and (ghi or jkl<t22)" | tee tmp.sh)
+    echo -e "(\n\
+           mpc search artist abc|grep -i -v \"t11\" \n\
+           mpc search artist def|grep -i \"t22\")" | diff -B -w - tmp.sh
+    assertEquals "0" "$?"
+}
+
+test_filter_prec_and_or_poss() {
+    cmd=$($SMPL_HOME/smartplaylist.sh -m spsh "(abc or def>t11) or (ghi or jkl<t22)" | tee tmp.sh)
+    echo -e "((\n\
+           mpc search artist abc\n\
+           mpc search artist def)|grep -i -v \"t11\"( \n\
+           mpc search artist ghi\n\
+           mpc search artist jkl)|grep -i \"t22\")" | diff -B -w - tmp.sh
+    assertEquals "0" "$?"
+}
+
 test_cleanup() {
     rm -f tmp.sh
 }

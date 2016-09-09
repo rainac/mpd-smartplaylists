845a70a7146d8c558156674ca02fb926f18145ea
rainac <johannes@johannes-willkomm.de>
2016-09-09 21:57:44 +0200

    Latest changes

--- wmpd/mpdview/static/js/mpdview.js
+++ wmpd/mpdview/static/js/mpdview.js
@@ -36,7 +36,7 @@ wmpc.PostprocGenHTML = function(txt) {
     }
 }
 
-wmpc.Request = function(method, url, data, callback) {
+wmpc.Request = function(method, url, data, callback, error) {
     d = new Object();
     d.request = wmpc.CreateRequest();
     d.request.open(method, url, true);
@@ -45,7 +45,9 @@ wmpc.Request = function(method, url, data, callback) {
         switch(d.request.readyState) {
         case 4:
             if (d.request.status != 200) {
-                alert("Error: HTTP Status: " + d.request.status);
+                console.warn("WMPC: HTTP status: " + d.request.status);
+                if (typeof error == "function")
+                    error(d.request.status, d)
             } else {
                 callback(d.request.responseText);
             }
@@ -56,7 +58,7 @@ wmpc.Request = function(method, url, data, callback) {
 }
 
 wmpc.Get = function(url, callback) {
-    console.log('wmpc.Get: ' + url);
+//    console.log('wmpc.Get: ' + url);
     return wmpc.Request('GET', url, '', callback);
 }
 
@@ -88,23 +90,78 @@ wmpc.mkLogOutFunc = function(name, gDone) {
     return res
 }
 
-wmpc.updateStatusTxt = function (target) {
+wmpc.intervals = {}
+wmpc.clearInterval = function (name) {
+    console.log('clear inter: ' + name)
+    clearInterval(wmpc.intervals[name])
+    wmpc.intervals[name] = undefined
+}
+wmpc.clearIntervals = function () {
+    for (k in wmpc.intervals) {
+        console.log(k)
+        console.log(typeof k)
+        console.log(typeof wmpc[k])
+        wmpc.clearInterval(k)
+    }
+    wmpc.intervals = {}
+}
+wmpc.addInterval = function(name, intervalHandle) {
+    console.log('adding inter: ' + name)
+    if (wmpc.intervals[name] != undefined) {
+        wmpc.clearInterval(name)
+    }
+    console.log('add inter: ' + name)
+    wmpc.intervals[name] = intervalHandle
+}
+
+wmpc.updateStatus = function (targetTxt, targetFancy, xproc) {
     wmpc.Get('ajax/mpc?cmd=status', function(status) {
-        xlp.attach(target, xlp.mkpre(status))
+        xlp.attach(document.getElementById(targetTxt), xlp.mkpre(status))
+        if (targetFancy != undefined) {
+            xproc.transform(status, function(result) {
+                xlp.attach(document.getElementById(targetFancy), result)
+            })
+        }
     });
 }
-wmpc.setupStatusTxt = function (target) {
+wmpc.setupStatusTxt = function (targetTxt, targetFancy, xproc) {
+    wmpc.updateStatusTxtCB = function() {
+        wmpc.updateStatus(targetTxt, targetFancy, xproc)
+    }
     wmpc.updateStatusTxtCB()
-    setInterval(function() {
-        wmpc.updateStatusTxt(target)
-    }, 1000)
+    wmpc.addInterval('status', setInterval(function() {
+        wmpc.updateStatusTxtCB()
+    }, 1000))
 }
-wmpc.updateStatusTxtCB = function () {
-    wmpc.updateStatusTxt(document.getElementById('wmpc-div-status-txt'))
+
+wmpc.updatePlaylist = function (targetTxt, targetFancy, xproc) {
+    wmpc.Get('ajax/mpc?cmd=playlist', function(status) {
+        xlp.attach(document.getElementById(targetTxt), xlp.mkpre(status))
+        if (targetFancy != undefined) {
+            xproc.transform(status, function(result) {
+                xlp.attach(document.getElementById(targetFancy), result)
+            })
+        }
+    });
+}
+wmpc.setupPlaylist = function (targetTxt, targetFancy, xproc) {
+    wmpc.updatePlaylistCB = function() {
+        wmpc.updatePlaylist(targetTxt, targetFancy, xproc)
+    }
+    wmpc.updatePlaylistCB()
+    wmpc.addInterval('playlist', setInterval(function() {
+        wmpc.updatePlaylistCB()
+    }, 10000))
 }
 
 wmpc.status = wmpc.mkLogOutFunc('')
-wmpc.play = wmpc.mkLogOutFunc('play', wmpc.updateStatusTxtCB)
+wmpc.play = function(index) {
+    cmd = 'play'
+    if (index != undefined) {
+        cmd = cmd + ' ' + index;
+    }
+    wmpc.Get('ajax/mpc?cmd=' + cmd, function(status) {})
+}
 wmpc.pause = wmpc.mkLogOutFunc('pause', wmpc.updateStatusTxtCB)
 wmpc.next = wmpc.mkLogOutFunc('next', wmpc.updateStatusTxtCB)
 wmpc.prev = wmpc.mkLogOutFunc('prev', wmpc.updateStatusTxtCB)
@@ -125,9 +182,92 @@ wmpc.replaygainplain = wmpc.mkLogOutFunc('replaygain')
 
 wmpc.update = wmpc.mkNoOutFunc('update')
 
+wmpc.seek_click = function(el, ev) {
+    cmd = 'seek'
+    console.dir(el)
+    console.dir(ev)
+    posdef = Math.round(10000 * (ev.layerX -  el.offsetLeft - 1) / el.clientWidth) / 100.0
+    wmpc.Get('ajax/mpc?cmd=seek+' + posdef + '%', function(status) {})
+}
+
+wmpc.evh = function(el, ev, func, args) {
+    func(args)
+//    ev.canceBubble = true
+}
+
 function WMPCMain(x, ev) {
 
     var mainproc = xlp.mkXLP(['mpdview.xsl'], '/static/xsl/')
+
+        var TOKEN_NA = 1111
+        var scanConf = {
+        name: 'Test',
+        ignored: 1,
+        rules: [
+            { re: ':', action: TOKEN_COLON },
+            { re: '/', action: TOKEN_DIV },
+            { re: '( |\t)+', action: TOKEN_SPACE },
+            { re: '\n', action: TOKEN_NEWLINE },
+            { re: '@', action: TOKEN_AT },
+            { re: '\\.', action: TOKEN_FULL_STOP },
+            { re: '#', action: TOKEN_HASH },
+            { re: '%', action: TOKEN_MOD },
+            { re: '[0-9]+', action: TOKEN_INTEGER },
+            { re: '[0-9]+\\.[0-9]+', action: TOKEN_FLOAT },
+            { re: '[a-zA-Z_-][a-zA-Z0-9_-]*', action: TOKEN_IDENTIFIER },
+            { re: 'n/a', action: TOKEN_NA },
+        ]
+    }
+    var parseConf = [
+        { type: TOKEN_ROOT, mode: MODE_UNARY, prec: 1 },
+        { type: TOKEN_NEWLINE, mode: MODE_BINARY, assoc: ASSOC_RIGHT, prec: 90, merged: true },
+        { type: TOKEN_JUXTA, mode: MODE_BINARY, assoc: ASSOC_RIGHT, prec: 100 },
+        { type: TOKEN_HASH, mode: MODE_UNARY, prec: 130 },
+        { type: TOKEN_MOD, mode: MODE_POSTFIX, prec: 132 },
+        { type: TOKEN_DIV, mode: MODE_BINARY, assoc: ASSOC_LEFT, prec: 135 },
+        { type: TOKEN_COLON, mode: MODE_BINARY, assoc: ASSOC_LEFT, prec: 140 },
+        { type: TOKEN_TAB, mode: MODE_IGNORE, prec: 2 },
+        { type: TOKEN_SPACE, mode: MODE_IGNORE, prec: 2 },
+        { type: TOKEN_ILLEGAL_CHAR, mode: MODE_IGNORE, prec: 2 },
+        { type: TOKEN_IGNORE, mode: MODE_IGNORE, prec: 2 }
+    ]
+    var p2xConf = {
+        scanner: scanConf,
+        parser: parseConf,
+        treeprinter: {},
+        debug: true
+    }
+
+//    var p2x = p2x.create(p2xConfig)
+    var p2xproc = {}
+    p2xproc.transform = function(txt, done) {
+        // console.log('p2xproc: input: ' + txt)
+        // xmltxt = "<fancy-status/>"
+        xmltxt = P2X.p2xj(txt, p2xConf)
+//        console.log('p2xproc: XML: ' + xmltxt)
+        mainproc.transformTxt(xmltxt, function(statusxml) {
+//            console.log('p2xproc: MPD status XML::: ' + xlp.serializeNode(statusxml))
+            mainproc.transform(statusxml, function(htmlfrag) {
+//                console.log('p2xproc: HTMLFragment::: ' + xlp.serializeNode(htmlfrag))
+                done(htmlfrag)
+            })
+        })
+    }
+
+    var p2xproc_playlist = {}
+    p2xproc_playlist.transform = function(txt, done) {
+        // console.log('p2xproc: input: ' + txt)
+        // xmltxt = "<fancy-status/>"
+        xmltxt = P2X.p2xj(txt, p2xConf)
+//        console.log('p2xproc: XML: ' + xmltxt)
+        mainproc.transformTxt('<p2xplaylist>'+xmltxt+'</p2xplaylist>', function(statusxml) {
+//            console.log('p2xproc: MPD status XML::: ' + xlp.serializeNode(statusxml))
+            mainproc.transform(statusxml, function(htmlfrag) {
+                // console.log('p2xproc: HTMLFragment::: ' + xlp.serializeNode(htmlfrag))
+                done(htmlfrag)
+            })
+        })
+    }
 
     wmpc.Get('ajax/', function(status) {
         console.log('status XML: ' + status)
@@ -135,7 +275,10 @@ function WMPCMain(x, ev) {
             console.log('result HTML: ' + htmlfrag)
             mainproc.attach(document.getElementById('wmpc-body'), htmlfrag)
 
-            wmpc.setupStatusTxt(document.getElementById('wmpc-div-status-txt'))
+            setTimeout(function () {
+                wmpc.setupPlaylist('wmpc-div-playlist-txt', 'wmpc-div-playlist-fancy', p2xproc_playlist)
+            }, 100)
+            wmpc.setupStatusTxt('wmpc-div-status-txt', 'wmpc-div-status-fancy', p2xproc)
         })
     });
 
--- wmpd/mpdview/static/xsl/mpdview.xsl
+++ wmpd/mpdview/static/xsl/mpdview.xsl
@@ -7,8 +7,12 @@ Copyright © 2014 Johannes Willkomm
     xmlns:ca='http://johannes-willkomm.de/xml/code-xml/attributes/'
     xmlns:worg='http://johannes-willkomm.de/xml/web/'
     xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:s='http://johannes-willkomm.de/xml/wmpc/status'
+    xmlns:p='http://johannes-willkomm.de/xml/wmpc/playlist'
     xmlns="http://www.w3.org/1999/xhtml"
     >
+
+  <xsl:variable name="statusns" select="'http://johannes-willkomm.de/xml/wmpc/status'"/>
 
   <xsl:output method="xml" indent="yes"/>
 
@@ -39,9 +43,25 @@ Copyright © 2014 Johannes Willkomm
   </xsl:template>
 
   <xsl:template match="status">
+    <div class="wmpc-status">
+      <xsl:apply-templates/>
+    </div>
+  </xsl:template>
+
+  <xsl:template match="text-status">
     <div id="wmpc-div-status-txt">
     </div>
-    <div id="wmpc-div-status">
+  </xsl:template>
+
+  <xsl:template match="text-playlist">
+    <div id="wmpc-div-playlist-txt">
+      <xsl:apply-templates/>
+    </div>
+  </xsl:template>
+
+  <xsl:template match="fancy-playlist">
+    <div id="wmpc-div-playlist-fancy">
+      <xsl:apply-templates/>
     </div>
   </xsl:template>
 
@@ -49,5 +69,171 @@ Copyright © 2014 Johannes Willkomm
     <div id="wmpc-div-log">
     </div>
   </xsl:template>
+
+  <xsl:template match="fancy-status">
+    <div id="wmpc-div-status-fancy">
+      <xsl:apply-templates/>
+    </div>
+  </xsl:template>
+
+  <xsl:template match="cx:root">
+    <s:status>
+      <xsl:text>&#xa;</xsl:text>
+      <xsl:apply-templates mode="status-xml"/>
+    </s:status>
+  </xsl:template>
+
+  <xsl:template match="text()" mode="status-xml"/>
+  <xsl:template match="text()" mode="status-xml-line1"/>
+  <xsl:template match="text()" mode="status-xml-line2"/>
+  <xsl:template match="text()" mode="status-xml-line3"/>
+  <xsl:template match="text()" mode="pos"/>
+  <xsl:template match="text()" mode="timepos"/>
+
+  <xsl:template match="cx:op[@type = 'NEWLINE']/cx:*" mode="status-xml-line1">
+    <s:title><xsl:value-of select="normalize-space(.)"/></s:title>
+    <xsl:text>&#xa;</xsl:text>
+  </xsl:template>
+  <xsl:template match="cx:op[@type = 'NEWLINE']/cx:*" mode="status-xml-line2">
+    <xsl:apply-templates mode="status-xml-line2"/>
+  </xsl:template>
+  <xsl:template match="cx:op[@type = 'NEWLINE']/cx:*" mode="status-xml-line3">
+    <xsl:apply-templates mode="status-xml-line3"/>
+  </xsl:template>
+
+  <xsl:template match="cx:op[@type = 'NEWLINE']/cx:*[1]" mode="status-xml">
+    <xsl:apply-templates select="." mode="status-xml-line1"/>
+  </xsl:template>
+
+  <xsl:template match="cx:op[@type = 'NEWLINE']/cx:op[@type = 'NEWLINE']/cx:*[1]" mode="status-xml">
+    <xsl:apply-templates select="." mode="status-xml-line2"/>
+  </xsl:template>
+
+  <xsl:template match="cx:op[@type = 'NEWLINE']/cx:op[@type = 'NEWLINE']/cx:*[2]" mode="status-xml">
+    <s:flags>
+      <xsl:apply-templates select="." mode="status-xml-line3"/>
+    </s:flags>
+  </xsl:template>
+
+  <xsl:template match="cx:op[@type = 'MOD']" mode="status-xml-line2">
+    <s:percentage index="{cx:*[1]}"/>
+    <xsl:text>&#xa;</xsl:text>
+  </xsl:template>
+
+  <xsl:template match="cx:op[@type = 'DIV']" mode="status-xml-line2">
+    <s:tpos>
+      <s:index>
+        <xsl:apply-templates select="cx:*[1]" mode="status-xml-line2"/>
+      </s:index>/<s:max>
+        <xsl:apply-templates select="cx:*[2]" mode="status-xml-line2"/>
+      </s:max>
+    </s:tpos>
+    <xsl:text>&#xa;</xsl:text>
+  </xsl:template>
+
+  <xsl:template match="cx:op[@type = 'COLON']" mode="status-xml-line2">
+    <s:time>
+      <s:min><xsl:value-of select="cx:*[1]"/></s:min>:<s:sec><xsl:value-of select="cx:*[2]/ca:text"/></s:sec>
+    </s:time>
+  </xsl:template>
+
+  <xsl:template match="cx:op[@type = 'HASH']" mode="status-xml-line2">
+    <s:lpos>
+      <s:index><xsl:value-of select="cx:*/cx:*[1]"/></s:index>/<s:max><xsl:value-of select="normalize-space(cx:*/cx:*[2])"/></s:max>
+    </s:lpos>
+    <xsl:text>&#xa;</xsl:text>
+  </xsl:template>
+
+  <xsl:template match="cx:op[@type = 'COLON']" mode="status-xml-line3">
+    <xsl:element name="{cx:*[1]}" namespace="{$statusns}">
+      <xsl:value-of select="cx:*[2]/ca:text"/>
+    </xsl:element>
+  </xsl:template>
+
+
+  <xsl:template match="s:status">
+    <div id="wmpc-status">
+      <xsl:apply-templates select="s:title"/>
+      <table class="wmpc-display">
+        <tr>
+          <td>
+            <xsl:apply-templates select="s:lpos"/>
+          </td>
+          <td>
+            <xsl:apply-templates select="s:tpos"/>
+          </td>
+          <td>
+            <xsl:apply-templates select="s:flags"/>
+          </td>
+        </tr>
+        <tr>
+          <th>Position</th>
+          <th>Time</th>
+          <th>Flags</th>
+        </tr>
+      </table>
+      <xsl:apply-templates select="s:percentage"/>
+    </div>
+  </xsl:template>
+
+  <xsl:template match="s:*">
+    <span class="wmpc-status-{local-name()}">
+      <xsl:apply-templates/>
+    </span>
+  </xsl:template>
+
+
+  <xsl:template match="s:flags">
+    <span class="wmpc-status-{local-name()}">
+      <xsl:for-each select="s:*">
+        <xsl:apply-templates select="."/>
+        <xsl:text> </xsl:text>
+      </xsl:for-each>
+    </span>
+  </xsl:template>
+
+  <xsl:template match="s:volume|s:random|s:single|s:consume|s:repeat">
+    <span onclick="wmpc.{local-name()}()" class="wmpc-status-{local-name()}">
+      <span class="wmpc-status-flag"><xsl:value-of select="local-name()"/></span>
+      <xsl:text>: </xsl:text>
+      <xsl:apply-templates/>
+    </span>
+  </xsl:template>
+
+  <xsl:template match="s:percentage">
+    <span class="wmpc-status-{local-name()}" onclick="wmpc.seek_click(this, event)">
+      <span class="wmpc-status-{local-name()}-passed" style="width: {@index}%"> </span>
+    </span>
+  </xsl:template>
+
+
+  <xsl:template match="p2xplaylist">
+    <p:list>
+      <xsl:apply-templates mode="playlist-xml"/>
+    </p:list>
+  </xsl:template>
+
+  <xsl:template match="text()" mode="playlist-xml"/>
+
+  <xsl:template match="cx:op[@type = 'NEWLINE']/cx:*[1]" mode="playlist-xml">
+    <p:item><xsl:value-of select="normalize-space(.)"/></p:item>
+    <xsl:text>&#xa;</xsl:text>
+  </xsl:template>
+
+
+  <xsl:template match="p:list">
+    <ol>
+      <xsl:apply-templates select="p:item"/>
+    </ol>
+  </xsl:template>
+
+  <xsl:template match="p:item">
+    <li>
+      <span onclick="wmpc.evh(this, event, wmpc.play, {position()});">
+        <xsl:apply-templates/>
+      </span>
+    </li>
+  </xsl:template>
+
 
 </xsl:stylesheet>

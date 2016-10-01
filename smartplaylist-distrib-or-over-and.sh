29ab255b18c841f1b628dbb9dc4c19a21385faa6
rainac <johannes@johannes-willkomm.de>
2016-10-01 09:48:44 +0200

    Raise error is fails to converge

--- smartplaylist-distrib-or-over-and.sh
+++ smartplaylist-distrib-or-over-and.sh
@@ -3,6 +3,9 @@
 res=0
 cat - > in.xml
 
+maxiter=100
+iter=0
+
 while ! xsltproc $SMPL_HOME/smartplaylist-has-paren.xsl in.xml >& /dev/null; do
     xsltproc -o in-tmp.xml $SMPL_HOME/smartplaylist-distrib-or.xsl in.xml
     res=$?
@@ -11,9 +14,17 @@ while ! xsltproc $SMPL_HOME/smartplaylist-has-paren.xsl in.xml >& /dev/null; do
         break
     fi
     cp in-tmp.xml in.xml
+    iter=$(( $iter + 1 ))
+    if [[ $iter -gt $maxiter ]]; then
+        echo "$(basename $0): error: maximum number of iterations reached" >&2
+        res=1
+        break
+    fi
 done
 
-cat in.xml
+if [[ "$res" = 0 ]]; then
+    cat in.xml
+fi
 rm -f in.xml in-tmp.xml
 
 exit $res
